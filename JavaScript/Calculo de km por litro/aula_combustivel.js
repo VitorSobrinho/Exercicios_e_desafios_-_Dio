d808b18a0862bd29bdde828146d247998b657b18
Vitor Sobrinho <95102525+VitorSobrinho@users.noreply.github.com>
2022-11-18 18:28:31 -0300

    Update aula_combustivel.js
    Alterações nos comentários do código

--- JavaScript/Calculo de km por litro/aula_combustivel.js
+++ JavaScript/Calculo de km por litro/aula_combustivel.js
@@ -1,16 +1,6 @@
 
-/*
-Faça um programa para calcular o valor gasto de combustivel em uma viagem.
+// Programa em JS para calcular o valor gasto de combustivel em uma viagem.
 
-São 3 variáveis:
-
-1 - Preço do combustível;
-2 - Gasto médio de combustível do carro por KM;
-3 - Distância em KM da viagem.
-
-Imprimindo no console, o valor que será gasto de combustível para realizar esta viagem.
-
-*/
 
 var precoCombustivel = 5.95; //Considerado que o valor não mudará
 var gastoPorKM = 12.5;
